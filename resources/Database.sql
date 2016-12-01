23453fe89250f4acf7ebbb98b163f01495587c62
anders11 <anderslatif@gmail.com>
2016-12-01 15:36:13 +0100

    Committing everything again. Fresh start.

--- resources/Database.sql
+++ resources/Database.sql
@@ -1,143 +1,30 @@
 /*
 Server: sql7.freemysqlhosting.net
-Name: sql7145733
-Username: sql7145733
-Password: WSeC627snV
+Name: sql7147049
+Username: sql7147049
+Password: 9TSw6r1nzu
 Port number: 3306
 */
 
-DROP SCHEMA IF EXISTS `sql7145733`;
-CREATE SCHEMA IF NOT EXISTS `sql7145733` DEFAULT CHARACTER SET utf8 ;
-USE `sql7145733`;
--- -----------------------------------------------------
--- Table `sql7145733`.`devicelogins`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`devicelogins` ;
 
-CREATE TABLE IF NOT EXISTS `sql7145733`.`devicelogins` (
-  `email` VARCHAR(40) NOT NULL,
-  `password` VARCHAR(100) NULL,
-  PRIMARY KEY (`email`)
+-- -----------------------------------------------------
+-- Schema DeviceGuru
+-- -----------------------------------------------------
+
+DROP SCHEMA IF EXISTS `sql7147295`;
+CREATE SCHEMA IF NOT EXISTS `sql7147295` DEFAULT CHARACTER SET utf8 ;
+USE `sql7147295` ;
+
+-- -----------------------------------------------------
+-- Table `DeviceGuru`.`Employees`
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS `sql7147295`.`students`;
+CREATE TABLE IF NOT EXISTS `sql7147295`.`students` (
+  `id` BIGINT(10) NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(50) NULL DEFAULT NULL,
+  `lastname` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
-
--- -----------------------------------------------------
--- Table `sql7145733`.`devices`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`devices` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`devices` (
-  `serialnumber` VARCHAR(30) NOT NULL,
-  `type` VARCHAR(20) NULL,
-  PRIMARY KEY (`serialnumber`)
-);
-
-
--- -----------------------------------------------------
--- Table `sql7145733`.`classes`
--- -----------------------------------------------------
-
-DROP TABLE IF EXISTS `sql7145733`.`classes` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`classes` (
-  `class` VARCHAR(5) NOT NULL,
-  PRIMARY KEY (`class`)
-);
-
-
-
--- -----------------------------------------------------
--- Table `sql7145733`.`students`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`students` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`students` (
-  `firstname` VARCHAR(30) NOT NULL,
-  `lastname`  VARCHAR(45) NOT NULL,
-  `class`     VARCHAR(5)  NOT NULL,
-  `device`    VARCHAR(30) NOT NULL,
-  `email`     VARCHAR(30) NOT NULL,
-  INDEX `deasd_idx` (`device` ASC),
-  INDEX `class_idx` (`class` ASC),
-  PRIMARY KEY (`email`),
-  FOREIGN KEY (`email`) REFERENCES devicelogins(`email`),
-  FOREIGN KEY (`device`) REFERENCES devices(`serialnumber`),
-  FOREIGN KEY (`class`) REFERENCES classes(`class`)
-
-);
-
--- -----------------------------------------------------
--- Table `sql7145733`.`personel`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`personel` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`personel` (
-  `firstname` VARCHAR(30) NOT NULL,
-  `lastname` VARCHAR(45) NOT NULL,
-  `device` VARCHAR(30) NOT NULL,
-  `email` VARCHAR(60) NOT NULL,
-  INDEX `device_idx` (`device` ASC),
-  INDEX `email_idx` (`email` ASC),
-  PRIMARY KEY (`email`),
-    FOREIGN KEY (`device`) REFERENCES devices(`serialnumber`),
-    FOREIGN KEY (`email`) REFERENCES devicelogins(`email`)
-);
-
--- -----------------------------------------------------
--- Table `sql7145733`.`repairs`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`repairs` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`repairs` (
-  `device` VARCHAR(30) NOT NULL,
-  `owner` VARCHAR(30) NOT NULL,
-  `ticketid` INT,
-  `status` INT NOT NULL,
-  PRIMARY KEY (`ticketid`),
-    FOREIGN KEY (`device`) REFERENCES devices(`serialnumber`),
-    FOREIGN KEY (`owner`) REFERENCES students(`email`)
-#  ,   FOREIGN KEY (`owner`) REFERENCES personel(`email`)
-);
-
--- -----------------------------------------------------
--- Table `sql7145733`.`logins`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`logins` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`logins` (
-  `username` VARCHAR(40) NOT NULL,
-  `password` VARCHAR(40) NOT NULL,
-  PRIMARY KEY (`username`)
-);
-
--- -----------------------------------------------------
--- Table `sql7145733`.`assignments`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`assignments` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`assignments` (
-  `assignment` VARCHAR(100) NOT NULL,
-  `owner` VARCHAR(40) NOT NULL,
-  `id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`owner`) REFERENCES logins(`username`)
-
-);
-
--- -----------------------------------------------------
--- Table `sql7145733`.`devicehistory`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sql7145733`.`devicehistory` ;
-
-CREATE TABLE IF NOT EXISTS `sql7145733`.`devicehistory` (
-  `email`     VARCHAR(30) NOT NULL,
-  `device`    VARCHAR(30) NOT NULL,
-  `startdate` DATE        NOT NULL,
-  `enddate`   DATE,
-  PRIMARY KEY (`email`),
-  FOREIGN KEY (`email`) REFERENCES students (`email`),
-#   FOREIGN KEY (`email`) REFERENCES personel (`email`),
-  FOREIGN KEY (`device`) REFERENCES devices (`serialnumber`)
-);
-
 
 
