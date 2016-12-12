398b7bd2e8b4332f8d096280453c69391ed1d1ff
Mikkel Thore Simonsen <m.thore@hotmail.com>
2016-12-12 23:24:51 +0100

    The appication now reads students from DB and adds them to the tableview.

--- src/main/java/javaFX/ui/MainTabController.java
+++ src/main/java/javaFX/ui/MainTabController.java
@@ -2,11 +2,18 @@ package javaFX.ui;
 
 import javaFX.models.Assignment.AssignmentService;
 import javaFX.models.Login.LoginService;
+import javaFX.models.Student.Student;
 import javaFX.models.Student.StudentService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Component
@@ -16,21 +23,67 @@ public class MainTabController {
     @Autowired
     StudentService studentService;
 
+    @FXML
+    TableView<Student> studentTable;
+
 
     @FXML
     public void initialize() {
 
-        /**
-         * Hej Mikkel.. Dette er muligt at få fat i alle elever.. returnerer List<Student>
-         * Der er også et rodet eksempel nedenunder
-         */
-        studentService.findAllStudents();
 
-        // se output i konsolen!
-        studentService.findAllStudents().forEach( s -> System.out.println(s.toString()));
+        //loads all student objects from the databases student tabel.
+        ArrayList<Student> studentsArray = (ArrayList<Student>)studentService.findAllStudents();
+        ObservableList<Student> studentList = FXCollections.observableArrayList(studentsArray);
+
+
+        createStudentTable();
+        loadStudents(studentList);
+
+    }
+
+    //creating the tablecolumns and defines them.
+    public void createStudentTable(){
+
+        //creating firstNamecolumn
+        TableColumn<Student, String> firstNameColumn = new TableColumn<>("Fornavn");
+        firstNameColumn.setMinWidth(200);
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+        //creating lastnamecolumn
+        TableColumn<Student, String> lastNameColumn = new TableColumn<>("Efternavn");
+        lastNameColumn.setMinWidth(200);
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+        //creating emailcolumn
+        TableColumn<Student, String> emailColumn = new TableColumn<>("Email");
+        emailColumn.setMinWidth(200);
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        //creating emailcolumn
+        TableColumn<Student, String> classColoumn = new TableColumn<>("Klasse");
+        classColoumn.setMinWidth(200);
+        classColoumn.setCellValueFactory(new PropertyValueFactory<>("schoolClass"));
+
+        //creating devicecolumn
+        TableColumn<Student, String> deviceColoumn = new TableColumn<>("Device");
+        deviceColoumn.setMinWidth(200);
+        deviceColoumn.setCellValueFactory(new PropertyValueFactory<>("device"));
+
+        //adding columns
+        studentTable.getColumns().addAll(firstNameColumn, lastNameColumn, emailColumn, classColoumn, deviceColoumn);
 
 
     }
+
+
+    //setting content in tableView
+    public void loadStudents(ObservableList<Student> studentList) {
+
+        studentTable.setItems(studentList);
+
+    }
+
+
 
 
 
